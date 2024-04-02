4bd1440b004144696031bd0e3d4bb158994bc261
abhiishekk02 <80751160+abhiishekk02@users.noreply.github.com>
2024-04-02 17:51:48 +0530

    index.js

--- index.js
+++ index.js
@@ -1,7 +1,35 @@
-const express = require('express')
-const app = express()
+
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const CertificateRouter = require("./routes/CertificateRoute");
+const ShowCaseRouter = require("./routes/ShowCaseRoute");
+const UserAuthRouter = require("./routes/UserAuthRoute");
+const ContactFormRouter = require("./routes/ContactFormRoute");
+const ClientRouter = require("./routes/ClientRoutes");
+const AnalyticsRoute = require("./routes/AnalyticsRoute");
+const TodoListRoute = require("./routes/TodoListRoute");
+const app = express();
+
+app.use(morgan("dev"));
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.all('/', (req, res) => {
     console.log("Just got a request!")
-    res.send('Yo!')
+    res.send('hi!')
 })
-app.listen(process.env.PORT || 3000)
+// Mount the certificate router
+app.use("/certificate", CertificateRouter);
+app.use("/showcase", ShowCaseRouter);
+app.use("/userAuth", UserAuthRouter);
+app.use("/contactForm", ContactFormRouter);
+app.use("/clientDetails", ClientRouter);
+app.use("/analytics", AnalyticsRoute);
+app.use("/todolist", TodoListRoute);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
